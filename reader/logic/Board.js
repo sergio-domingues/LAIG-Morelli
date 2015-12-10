8b749035dd83d11d7fa048c45e9e789e2eac6cca
Diogo Moura <diogomoura13@gmail.com>
2015-12-10 15:57:45 +0000

    diff function created and now can conquist pieces and throne

--- reader/logic/Board.js
+++ reader/logic/Board.js
@@ -3,7 +3,11 @@ function Board(scene, size) {
     this.scene = scene;
     this.board = [];
     this.logicBoard = [];
-    this.logicBoardInitial=[];
+    this.logicBoardInitial = [];
+    
+    this.pieceSelected = [];
+    this.pathHighlighted = [];
+    
     this.init();
 }
 
@@ -37,6 +41,7 @@ Board.prototype.init = function() {
         var pos = this.getCoords(i);
         var colorIndexY = pos["y"]
           
+        
         , colorIndexX = pos["x"];
         
         if (pos["x"] > this.size / 2) {
@@ -57,7 +62,7 @@ Board.prototype.init = function() {
 }
 
 Board.prototype.initTab = function(data) {
-    this.logicBoardInitial=data;
+    this.logicBoardInitial = data;
     for (var y = 0; y < data.length; y++) {
         for (x = 0; x < data.length; x++) {
             if (data[y][x] == 0 || data[y][x] == 1) {
@@ -67,9 +72,16 @@ Board.prototype.initTab = function(data) {
     }
 }
 
-Board.prototype.highlightPath=function(array){
-    for(var i=0;i<array.length;i++){
-        this.board[(array[i][1]-1)*this.size+(array[i][0]-1)].setHighlighted();
+Board.prototype.highlightPath = function(array) {
+    for (var i = 0; i < array.length; i++) {
+        this.board[(array[i][1] - 1) * this.size + (array[i][0] - 1)].setHighlighted();
+    }
+    this.pathHighlighted = array;
+}
+
+Board.prototype.clearPath = function() {
+    for (var i = 0; i < this.pathHighlighted.length; i++) {
+        this.board[(this.pathHighlighted[i][1] - 1) * this.size + (this.pathHighlighted[i][0] - 1)].setHighlighted();
     }
 }
 
@@ -82,4 +94,69 @@ Board.prototype.getCoords = function(pos) {
         "y": y
     
     }
+}
+
+Board.prototype.movePiece = function(board) {
+
+    var difference=this.diff(this.logicBoardInitial,board);
+    console.log(difference);
+
+    var piece = this.logicBoard[difference["move"]["old"][1] * this.size + difference["move"]["old"][0]];
+    piece.x = difference["move"]["new"][0];
+    piece.y = difference["move"]["new"][1];
+    piece.setHighlighted();
+    this.logicBoard[difference["move"]["old"][1] * this.size + difference["move"]["old"][0]] = undefined;
+    this.logicBoard[difference["move"]["new"][1] * this.size + difference["move"]["new"][0]] = piece;
+    
+    if(difference["capture"].length>0){
+        var player=this.logicBoard[difference["capture"][1] * this.size + difference["capture"][0]].player;
+        this.logicBoard[difference["capture"][1] * this.size + difference["capture"][0]].player=(1-player);
+    }
+
+    if(difference["throne"]!=-1){
+        
+    }
+    
+    
+    this.clearPath();
+    this.logicBoardInitial=board;
+
+}
+
+Board.prototype.diff = function(tabOld, tabNew) {
+    
+    var newPos = [];
+    var oldPos = [];
+    var capture = [];
+    var piece;
+    
+    for (var i = 0; i < this.size; i++) {
+        for (var j = 0; j < this.size; j++) {
+            
+            if (tabOld[i][j] != tabNew[i][j]) {
+                if (tabOld[i][j] == -1 && tabNew[i][j] != -1) {
+                    piece = tabNew[i][j];
+                    newPos[0] = j;
+                    newPos[1] = i;
+                } else if (tabOld[i][j] != -1 && tabNew[i][j] == -1) {
+                    piece = tabNew[i][j];
+                    oldPos[0] = j;
+                    oldPos[1] = i;
+                } else if (tabOld[i][j] == (1 - tabNew[i][j])) {
+                    capture.push(j, i);
+                }
+            }
+        }
+    }
+    
+    return {
+        "move": {
+            "new": newPos,
+            "old": oldPos
+        },
+        "capture": capture,
+        "throne": tabNew[Math.floor(this.size / 2)][Math.floor(this.size / 2)]
+    
+    }
+
 }
--- reader/logic/GameState.js
+++ reader/logic/GameState.js
@@ -1,43 +1,77 @@
-function Morreli(scene,size,gamemode){
-    this.currentState="INIT";
-    this.player=0;
-    this.size=size;
-    this.board=new Board(scene,size);
-    this.connection=new Connection();
+function Morreli(scene, size, gamemode) {
+    this.currentState = "INIT";
+    this.player = 0;
+    this.size = size;
+    this.board = new Board(scene,size);
+    this.connection = new Connection();
     this.init();
 
 }
 
-Morreli.prototype.init=function(){
-    var self=this;
-   this.connection.initTabuleiro(this.size, function(board) {
-       self.board.initTab(board);
+Morreli.prototype.init = function() {
+    var self = this;
+    this.connection.initTabuleiro(this.size, function(board) {
+        self.board.initTab(board);
     });
-    //this.board.highlightPath([[2,2],[3,3],[4,4],[5,5],[6,6]]);
 }
 
-Morreli.prototype.display=function(){
+Morreli.prototype.display = function() {
     this.board.display();
 }
 
-Morreli.prototype.update=function(id,piece){
-    if(this.currentState=="INIT" &&id>200){
-        piece.setHighlighted();
-        this.getValidMoves(id);
-        this.currentState="PIECESELECT";
+Morreli.prototype.update = function(id, piece) {
+    //Selectionar peca no estdo inicial
+    if (this.currentState == "INIT" && id > 200) {
+        if (piece.player == this.player) {
+            piece.setHighlighted();
+            this.board.pieceSelected = [piece.x, piece.y];
+            this.getValidMoves(id);
+            this.currentState = "PIECESELECT";
+        }
+    } //Peca selecionada e carrega novamente numa peca
+    else if (this.currentState == "PIECESELECT" && id > 200) {
+        //volta a carregar na peca => volta para o estado inicial
+        if (piece.player == this.player) {
+            this.board.clearPath();
+            if (piece.x == this.board.pieceSelected[0] && piece.y == this.board.pieceSelected[1]) {
+                this.board.pieceSelected = [];
+                piece.setHighlighted();
+                this.currentState = "INIT";
+            } else {        //carrega noutra peca e mete highlighted
+                this.board.logicBoard[this.board.pieceSelected[1]*this.size+this.board.pieceSelected[0]].setHighlighted();
+                piece.setHighlighted();
+                this.board.pieceSelected = [piece.x, piece.y];
+                this.getValidMoves(id);
+                this.currentState = "PIECESELECT";
+            }
+        
+        }
     }
-
-    if(this.currentState=="PIECESELECT" && id<200){
-
+    //Peca selecionada e carrega numa posicao
+    if (this.currentState == "PIECESELECT" && id < 200) {
+        var coords=this.getCoords(id-1);
+        this.movePiece(coords.x,coords.y);
+        this.player = (1-this.player);
+        this.currentState="INIT";
     }
 }
 
-Morreli.prototype.getValidMoves=function(selected){
-    var self=this;
-    var pos= this.getCoords(selected-201);
-    this.connection.validMoves(this.board.logicBoardInitial,this.size,this.player,pos.x+1,pos.y+1, function(positions) {
-       self.board.highlightPath(positions);
+Morreli.prototype.getValidMoves = function(selected) {
+    var self = this;
+    var pos = this.getCoords(selected - 201);
+    this.connection.validMoves(this.board.logicBoardInitial, this.size, this.player, pos.x + 1, pos.y + 1, function(positions) {
+        self.board.highlightPath(positions);
     });
+}
+
+Morreli.prototype.movePiece=function(xf,yf){
+    var self=this;
+
+    this.connection.movePiece(this.board.logicBoardInitial,this.size,this.player,this.board.pieceSelected[0]+1,this.board.pieceSelected[1]+1,xf+1,yf+1,function(board){
+        self.board.movePiece(board);
+
+    });
+
 }
 
 Morreli.prototype.getCoords = function(pos) {
